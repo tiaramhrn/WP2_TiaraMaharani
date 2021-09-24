5705e352cec3dd26560d45ca96a2a73b77198e4d
tiaramhrn <tiaramhrn1005@gmail.com>
2021-09-24 10:10:39 +0700

    penambahan isi biodata

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Tiara Maharani, Saya tiggal di daerah Puncak";
+        echo "Nama Saya Tiara Maharani, Saya tiggal di daerah Puncak hobi saya adalah menonton";
     }
 }
