a2c0fbbb112821b74d230fb74641a3979eb4d923
tiaramhrn <tiaramhrn1005@gmail.com>
2021-10-07 21:42:55 +0700

    menambahkan file tugas per 3

---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Tiara</li>
+        <li>Nama Belakang : Maharani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Pancamarga Tugu Selatan, Cisarua</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Lari</li>
+        <li>Berenang</li>
+    </ul>
+</section>
+
+
+
